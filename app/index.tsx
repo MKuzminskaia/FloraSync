d97df5f7d3e342fe9c9d7e10a591fff7a76a3ad2
blackmarka <m.kuzminskaia@gmail.com>
2026-07-07 16:50:29 +0300

    feat: add first RNR components (Button, Text)
    - add components/ui/button.tsx and components/ui/text.tsx via RNR CLI
    
    - rewrite index.tsx to use new Button and Text instead of react-native defaults
    
    - add gap between buttons in index.tsx
    
    - update global.css

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+
 import { useRouter } from "expo-router";
-import { Button, View } from "react-native";
+import { View } from "react-native";
 
 export default function firstStack() {
   const router = useRouter();
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Button onPress={() => router.push("/plants")} title="Go to Plants" />
-      <Button
-        onPress={() => router.push("/add-plant")}
-        title="Add a new plant"
-      />
-      <Button
-        onPress={() => router.push("/calendar")}
-        title="Show plant's calendar"
-      />
-      <Button
-        onPress={() => router.push("/notifications")}
-        title="Show notifications"
-      />
+    <View className="flex-1 items-center justify-center gap-4">
+      <Button onPress={() => router.push("/plants")}>
+        <Text>Go to Plants</Text>
+      </Button>
+      <Button onPress={() => router.push("/add-plant")}>
+        <Text>Add a new plant</Text>
+      </Button>
+      <Button onPress={() => router.push("/calendar")}>
+        <Text>Show plant's calendar</Text>
+      </Button>
+      <Button onPress={() => router.push("/notifications")}>
+        <Text>Show notifications</Text>
+      </Button>
     </View>
   );
 }
