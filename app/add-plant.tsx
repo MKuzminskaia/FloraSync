888c2b538e63c42a0f2256bc0f24dddb9c8d2a18
blackmarka <m.kuzminskaia@gmail.com>
2026-07-13 23:09:45 +0300

    feat: connect add-plant screen to usePlants - handleSave now calls usePlants().addPlant to persist a new plant - install CLI component input components/ui/input.tsx - refresh package-lock.json, package.json

--- app/add-plant.tsx
+++ app/add-plant.tsx
@@ -1,5 +1,52 @@
-import { Text } from "react-native";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Text } from "@/components/ui/text";
+import { usePlants } from "@/hooks/use-plants";
+import { useState } from "react";
+
+import { ScrollView } from "react-native";
 
 export default function AddNewPlant() {
-  return <Text>add a new plant (soon) </Text>;
+  const [plantNickname, setPlantName] = useState<string>("");
+  const [plantLocation, setPlantLocation] = useState<string>("");
+  const { addPlant } = usePlants();
+
+  const handleSave = () => {
+    const newPlant = {
+      speciesId: null,
+      nickname: plantNickname,
+      location: plantLocation,
+      photoUrl: null,
+      potChangedAt: null,
+    };
+
+    addPlant(newPlant);
+  };
+
+  return (
+    <ScrollView
+      className="flex-1"
+      contentContainerClassName="flex-grow items-center justify-center gap-4 p-4"
+    >
+      <Text>Enter information about the new plant</Text>
+
+      <Text>Enter the name of the new plant *</Text>
+      <Input
+        placeholder="New plant name"
+        value={plantNickname}
+        onChangeText={setPlantName}
+      />
+
+      <Text>Enter the location of the new plant (Optional)</Text>
+      <Input
+        placeholder="New plant location (Where it is located)"
+        value={plantLocation}
+        onChangeText={setPlantLocation}
+      />
+
+      <Button onPress={handleSave}>
+        <Text>Add</Text>
+      </Button>
+    </ScrollView>
+  );
 }
