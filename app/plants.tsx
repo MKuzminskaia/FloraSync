6a632fc0e74e78497afb852861c60fafa49c2e79
blackmarka <m.kuzminskaia@gmail.com>
2026-07-14 19:46:58 +0300

    feat: render plant list on plants screen

--- app/plants.tsx
+++ app/plants.tsx
@@ -1,5 +1,23 @@
-import { Text } from "react-native";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { usePlants } from "@/hooks/use-plants";
+import { ScrollView } from "react-native";
 
 export default function ListOfPlants() {
-  return <Text>List of the plants (soon)</Text>;
+  const { plants } = usePlants();
+
+  return (
+    <ScrollView
+      className="flex-1"
+      contentContainerClassName="flex-grow items-center justify-center gap-4 p-4"
+    >
+      <Text>List of the plants (soon)</Text>
+      {plants.map((plant) => (
+        <Text key={plant.id}> Plant {plant.nickname} </Text>
+      ))}
+      <Button>
+        <Text>Refresh</Text>
+      </Button>
+    </ScrollView>
+  );
 }
