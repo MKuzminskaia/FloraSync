728a9d906af23bd0dcfd6c74ab2460eeed109c8d
blackmarka <m.kuzminskaia@gmail.com>
2026-07-18 17:22:55 +0300

    feat: display plant details on plant screen

--- app/plants/[id].tsx
+++ app/plants/[id].tsx
@@ -1,6 +1,7 @@
 import { Text } from "@/components/ui/text";
 import { getPlantById, Plant } from "@/lib/plants";
 import { useLocalSearchParams } from "expo-router";
+import { ScrollView } from "react-native";
 
 export default function PlantDetails() {
   const { id } = useLocalSearchParams();
@@ -8,5 +9,16 @@ export default function PlantDetails() {
 
   const plant: Plant | null = getPlantById(plantId);
   if (!plant) return <Text>Plant not found</Text>;
-  return <Text>This is your plant : {plant.nickname}</Text>;
+  return (
+    <ScrollView
+      className="flex-1"
+      contentContainerClassName="flex-grow items-center justify-center gap-4 p-4"
+    >
+      <Text>{plant.nickname} details:</Text>
+      <Text>Location : {plant.location ?? "-"}</Text>
+      <Text>Photo : {plant.photoUrl ?? "-"}</Text>
+      <Text>Pot info : {plant.potChangedAt ?? "-"}</Text>
+      <Text>Species info: {plant.speciesId ?? "-"}</Text>
+    </ScrollView>
+  );
 }
