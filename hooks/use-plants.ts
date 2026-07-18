9bb68c95c4d50a176f561527844d39463f98a021
blackmarka <m.kuzminskaia@gmail.com>
2026-07-19 00:15:30 +0300

    refactor: use useFocusEffect to refresh list of plants

--- hooks/use-plants.ts
+++ hooks/use-plants.ts
@@ -1,12 +1,20 @@
 import { addNewPlant, getAllPlants, Plant } from "@/lib/plants";
-import { useState } from "react";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
 
 export function usePlants() {
-  const [plants, setPlants] = useState<Plant[]>(() => getAllPlants());
+  const [plants, setPlants] = useState<Plant[]>([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      setPlants(getAllPlants());
+    }, []),
+  );
 
   const addPlant = (plant: Omit<Plant, "id">) => {
     addNewPlant(plant);
     setPlants(getAllPlants());
   };
+
   return { plants, addPlant };
 }
